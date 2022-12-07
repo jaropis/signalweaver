9ec3fd297d059fa97261cf1247ebe3b3921292a6
Jaroslaw Piskorski <jaropis@zg.home.pl>
2022-12-07 21:55:31 +0100

    some more corrections

--- dash_gui.py
+++ dash_gui.py
@@ -2,13 +2,13 @@ from app import app
 from signalweaver.callbacks import prepare_callbacks
 from signalweaver.ecgs.manager import add_ecg
 from signalweaver.layouts.main import prepare_layout
+import glob
 
 server = app.server
 
-name = "/home/jaropis/Dropbox/Praca2/staff/wyniki/AVA02_2017-01-11 i-.csv"
-#ecg = DashECGSignal(name)  # this is global an constant for now
 with app.server.app_context():
-    add_ecg(name)
+    first_file = glob.glob('data/*.csv')[0]
+    add_ecg(first_file)
 
 prepare_layout(app)
 #prepare_css(app)
