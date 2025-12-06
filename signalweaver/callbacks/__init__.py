bd098507edc0fabab5647540b0cf854f4007aa8e
Jaroslaw Piskorski <jaropis@zg.home.pl>
2025-12-06 21:19:03 +0100

    some cleanup

--- signalweaver/callbacks/__init__.py
+++
@@ -1,5 +0,0 @@
-def prepare_callbacks():
-    import signalweaver.callbacks.ecg
-    import signalweaver.callbacks.misc
-    import signalweaver.callbacks.plotting
-    import signalweaver.callbacks.poincare_plot
